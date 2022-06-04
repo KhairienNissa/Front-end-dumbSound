1d6267e0166cc20eb3102609737376fa6e01e111
KhairienNissa <cacakhairien@gmail.com>
2022-06-04 21:51:35 +0700

    update

--- src/pages/user/HomeUser.js
+++ src/pages/user/HomeUser.js
@@ -62,7 +62,7 @@ useEffect(() => {
         music.map((item) => {
              return (
                
-           <a onClick={()=> navigate('/transaksi-premium' )} className="col-md-2 mx-2 text-decoration-none text-white"> 
+           <a onClick={()=> navigate('/transaksi-premium' )} className="mx-sm-5 mx-3 borra-4 d-flex gap-3 justify-content-center flex-1 flex-wrap"> 
             <CardLagu item={item}/> </a> 
              )
                  })
@@ -77,7 +77,7 @@ useEffect(() => {
              <>
               {userTrans?.status === "success" &&
                <>
-                 <div className="mx-sm-5 mx-3 borra-4 d-flex gap-4 justify-content-center flex-1 flex-wrap">
+                 <div className="mx-sm-5 mx-3 borra-4 d-flex gap-3 justify-content-center flex-1 flex-wrap">
                {music?
                 music.map((item) => {
              return (
