e642cc42a28dde978079a6b9c3b01ab9b10fdd2c
KhairienNissa <cacakhairien@gmail.com>
2022-06-04 17:31:05 +0700

    update

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,14 +5,17 @@ import 'react-h5-audio-player/lib/styles.css'
 import { Navbar } from "react-bootstrap";
 import AudioPlayer from 'react-h5-audio-player'
 
-const path = "http://localhost:5000/uploads/"
+const path = "https://res.cloudinary.com/khairien/video/upload/v1654335135/"||
+            "http://localhost:5000/uploads/"
+
+const pathImg = "https://res.cloudinary.com/khairien/image/upload/v1654335135/"
 
 const Player = ({playMusic}) => {
     return (
         <div>
             <Navbar className="fixed-bottom navplay d-flex">
                     <div style={{paddingRight:"100px"}} className="d-flex w-50 justify-content-center me-5">
-                            <img src={path + playMusic.thumbnail} width="50px" height="50px" alt="artis" className="image-music rounded-circle"/>
+                            <img src={pathImg + playMusic.thumbnail} width="50px" height="50px" alt="artis" className="image-music rounded-circle"/>
                             <p className="text-white mt-3 ms-3">{playMusic.artis.name} - {playMusic.title}</p>
                     </div>
                     <AudioPlayer
