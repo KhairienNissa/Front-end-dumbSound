64a57ca9fa98b641b9c8c5960929e3dac9a9f7c6
KhairienNissa <cacakhairien@gmail.com>
2022-06-04 16:57:52 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
 export const API = () => {
   const baseUrl = 
-    process.env.REACT_APP_SERVER_URL ||
-    "https://caca-dumbsound.herokuapp.com/api/v1" ||
-    "http://localhost:5000/api/v1";
+    
+    "https://caca-dumbsound.herokuapp.com/api/v1"
+  
 
   const executeAPI = async (endpoint, config) => {
     const response = await fetch(baseUrl + endpoint, config);
